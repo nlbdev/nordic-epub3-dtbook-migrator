8a8df6aa72bd6b85e9de45db8533a581eff31492
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2017-09-19 14:56:57 +0200

    [nordic] fix project setup (pom.xml and XProcSpecTest.java)

--- src/test/java/XProcSpecTest.java
+++ src/test/java/XProcSpecTest.java
@@ -1,38 +1,43 @@
-import org.daisy.pipeline.junit.AbstractXSpecAndXProcSpecTest;
-
-import static org.daisy.pipeline.pax.exam.Options.mavenBundle;
-
-import org.ops4j.pax.exam.Configuration;
-import static org.ops4j.pax.exam.CoreOptions.composite;
-import static org.ops4j.pax.exam.CoreOptions.options;
-import org.ops4j.pax.exam.Option;
-
-public class XProcSpecTest extends AbstractXSpecAndXProcSpecTest {
-	
-	@Override
-	protected String[] testDependencies() {
-		return new String[] {
-			pipelineModule("epub3-ocf-utils"),
-            pipelineModule("epub3-pub-utils"),
-            pipelineModule("epub3-nav-utils"),
-            pipelineModule("html-utils"),
-            pipelineModule("dtbook-validator"),
-            pipelineModule("dtbook-utils"),
-            pipelineModule("fileset-utils"),
-            pipelineModule("file-utils"),
+import org.daisy.pipeline.junit.AbstractXSpecAndXProcSpecTest; 
+ 
+import static org.daisy.pipeline.pax.exam.Options.*; 
+ 
+import org.ops4j.pax.exam.Configuration; 
+import static org.ops4j.pax.exam.CoreOptions.composite; 
+import static org.ops4j.pax.exam.CoreOptions.options; 
+import org.ops4j.pax.exam.Option; 
+ 
+public class XProcSpecTest extends AbstractXSpecAndXProcSpecTest { 
+     
+    @Override 
+    protected String[] testDependencies() { 
+        return new String[] { 
             pipelineModule("asciimath-utils"),
+            pipelineModule("common-utils"),
+            pipelineModule("dtbook-utils"),
+            pipelineModule("dtbook-validator"),
+            pipelineModule("epub3-nav-utils"),
+            pipelineModule("epub3-ocf-utils"),
+            pipelineModule("epub3-pub-utils"),
             pipelineModule("epubcheck-adapter"),
-            "com.google.guava:guava:?"
-		};
-	}
-	
-	@Override @Configuration
-	public Option[] config() {
-		return options(
-			composite(super.config()),
-			
-			// for org.apache.httpcomponents:httpclient (<-- xmlcalabash):
-			mavenBundle("org.slf4j:jcl-over-slf4j:1.7.2")
-		);
-	}
-}
+            pipelineModule("file-utils"),
+            pipelineModule("fileset-utils"),
+            pipelineModule("html-utils"),
+            pipelineModule("mediatype-utils"),
+            pipelineModule("validation-utils")
+        }; 
+    } 
+     
+    @Override @Configuration 
+    public Option[] config() { 
+        return options( 
+            composite(super.config()), 
+            mavenBundlesWithDependencies( 
+                mavenBundle("com.google.guava:guava:?")
+            ), 
+             
+            // for org.apache.httpcomponents:httpclient (<-- xmlcalabash): 
+            mavenBundle("org.slf4j:jcl-over-slf4j:1.7.2") 
+        ); 
+    } 
+} 
