af4cdfb005bb3f5b2be5f39609ea426ea11065c6
Daniel Persson <mailto.woden@gmail.com>
2022-12-09 14:29:35 +0100

    Update the schema script so we add the 2015 RNG file.

--- client/createSchemas.sh
+++ client/createSchemas.sh
@@ -7,10 +7,12 @@ SCHEMA_PATH=$MIGRATOR_PATH/src/main/resources/xml/schema
 mkdir -p src/main/resources/2015-1
 mkdir -p src/main/resources/2020-1
 
-#cp $SCHEMA_PATH/2015-1/nordic-html5.rng src/main/resources/2015-1
-#sed -i 's/http:\/\/www.daisy.org\/pipeline\/modules\/mathml-utils\/mathml3.rng/mathml3.rng/' src/main/resources/2020-1/nordic-html5.rng
-#sed -i 's/<define name="annotation">/<define name="annotation" combine="choice">/' src/main/resources/2020-1/nordic-html5.rng
-#sed -i 's/<define name="list">/<define name="list" combine="choice">/' src/main/resources/2020-1/nordic-html5.rng
+cp $SCHEMA_PATH/2015-1/nordic-html5.rng src/main/resources/2015-1
+sed -i 's/http:\/\/www.daisy.org\/pipeline\/modules\/mathml-utils\/mathml3.rng/mathml3.rng/' src/main/resources/2015-1/nordic-html5.rng
+sed -i 's/<define name="annotation">/<define name="annotation" combine="choice">/' src/main/resources/2015-1/nordic-html5.rng
+sed -i 's/<define name="list">/<define name="list" combine="choice">/' src/main/resources/2015-1/nordic-html5.rng
+sed -i '/<start combine="choice">/,/<\/start>/d' src/main/resources/2015-1/nordic-html5.rng
+
 saxon9-xslt -s:$SCHEMA_PATH/2015-1/nordic2015-1.nav-ncx.sch -o:src/main/resources/2015-1/nordic2015-1.nav-ncx.xsl -xsl:$SCHXSLT_PATH/2.0/compile-for-svrl.xsl
 saxon9-xslt -s:$SCHEMA_PATH/2015-1/nordic2015-1.nav-references.sch -o:src/main/resources/2015-1/nordic2015-1.nav-references.xsl -xsl:$SCHXSLT_PATH/2.0/compile-for-svrl.xsl
 saxon9-xslt -s:$SCHEMA_PATH/2015-1/nordic2015-1.opf-and-html.sch -o:src/main/resources/2015-1/nordic2015-1.opf-and-html.xsl -xsl:$SCHXSLT_PATH/2.0/compile-for-svrl.xsl
